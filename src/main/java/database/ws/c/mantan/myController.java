531ab988e6f86388947f752ec184fc536f52eaa0
abianfikri <abronjoy46@gmail.com>
2023-01-06 09:56:54 +0700

    menambahkan POST PUT GET di myController dan Index

--- src/main/java/database/ws/c/mantan/myController.java
+++ src/main/java/database/ws/c/mantan/myController.java
@@ -10,6 +10,7 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import javax.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpEntity;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 import org.springframework.stereotype.Controller;
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
@@ -31,48 +33,19 @@ public class myController {
     C2022ws data = new C2022ws();
     C2022wsJpaController dtcrl = new C2022wsJpaController();
     
-    @RequestMapping("/getNama")
-    public String getNameById(){
-        
-        try{
-            data = dtcrl.findC2022ws(1);
-        }
-        catch(Exception error){
-            
-        }
-        
-        return data.getName();
-    }
-    
-    
-    @RequestMapping("/getBorn")
-    public String getBornById(){
-        
-        try{
-            
-            data = dtcrl.findC2022ws(1);
-            
-        }
-        catch(Exception error){
-            
-        }
-        
-        return "tanggal = "+ data.getBirthdate().toString();
-    }
-    
-    @RequestMapping("/getData")
+    @RequestMapping("/GET")
     public String getdataById(){
         
+        List<C2022ws> datas = new ArrayList<>();
+        
         try{
-            data = dtcrl.findC2022ws(1); 
+            datas = dtcrl.findC2022wsEntities(); 
         }
         catch(Exception error){
             
         }
         
-        String result = data.getName() + "<p/>" + data.getBirthdate().toString();
-        
-        return result;
+        return datas.toString();
     }
     
     @RequestMapping("/getData/{id}")
@@ -86,7 +59,7 @@ public class myController {
     }
     
     
-    @RequestMapping(value = "/sendData", method = RequestMethod.POST, consumes = APPLICATION_JSON_VALUE)
+    @RequestMapping(value = "/POST", method = RequestMethod.POST, consumes = APPLICATION_JSON_VALUE)
     public String sendData(HttpEntity<String> kiriman) throws Exception{
         String json_receive = kiriman.getBody();
         String message = "no action";
@@ -102,4 +75,32 @@ public class myController {
         
         return message;
     }
+    
+    @RequestMapping(value = "/PUT", method = RequestMethod.PUT, consumes = APPLICATION_JSON_VALUE)
+    public String editData(HttpEntity<String> kiriman) throws Exception{
+        String json_receive = kiriman.getBody();
+        String message = "no action";
+        
+        
+        ObjectMapper mapper = new ObjectMapper();
+        C2022ws data = new C2022ws();
+        
+        data= mapper.readValue(json_receive, C2022ws.class);
+        dtcrl.edit(data);
+        message = data.getName() + " Update";
+        
+        
+        return message;
+    }
+    
+    @RequestMapping(value = "/DEL/{id}", method = RequestMethod.DELETE, consumes = APPLICATION_JSON_VALUE)
+    public String deleteData(@RequestParam("id") int id) throws Exception{
+        
+        String message = "no action";
+        dtcrl.destroy(id);
+        message = id + " delete";
+        
+        return message;
+    }
+    
 }
